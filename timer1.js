9e5d6a288b5e81aeec7e1fc926eea40f68287a2e
Sneha Mahajan <sneh.km@gmail.com>
2022-04-15 17:49:28 -0400

    Add condition for ignoring -ve numbers

--- timer1.js
+++ timer1.js
@@ -2,13 +2,13 @@
 let args = process.argv.slice(2);
 
 // Filter anything that is not a number
-// and convert to seconds
+// or negative number and convert to seconds
 args = args
   .map((a) => Number.parseInt(a))
-  .filter((a) => Number.isInteger(a))
+  .filter((a) => Number.isInteger(a) && a > 0)
   .map((a) => a * 1000);
 
-console.log(args);
+// console.log(args);
 
 // Function implementation for timer()
 // Takes an array of numbers a beep is played at each interval.
