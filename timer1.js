d9f3d3de753c95a60abb3d30c59225d86472b670
Sneha Mahajan <sneh.km@gmail.com>
2022-04-15 17:42:18 -0400

    Add timer1.js file

--- timer1.js
+++ timer1.js
@@ -1,24 +1,23 @@
 // Receive arguments from the terminal
 let args = process.argv.slice(2);
 
-// Function implementation for timer:
-// Takes an array of numbers received as strings
-// from the terminal. A beep is played at each interval
-// in the array.
+// Filter anything that is not a number
+// and convert to seconds
+args = args
+  .map((a) => Number.parseInt(a))
+  .filter((a) => Number.isInteger(a))
+  .map((a) => a * 1000);
+
+console.log(args);
+
+// Function implementation for timer()
+// Takes an array of numbers a beep is played at each interval.
 const timer = (delay) => {
   delay.forEach((del) => {
-    // Convert each string element to number
-    del = Number.parseInt(del);
-
-    // Convert to seconds
-    del *= 1000;
-
-    // Call a setTimeout for each element of the array
     setTimeout(() => {
       process.stdout.write("\x07");
     }, del);
   });
 };
 
-// Function call for timer
 timer(args);
